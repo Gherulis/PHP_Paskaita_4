16a54d6b19d0883441f908f4c02a93f1b3ede279
Tomas Gruseckas <111756462+Gherulis@users.noreply.github.com>
2022-09-20 22:12:26 +0300

    Pabaigtas ND

--- index.php
+++ index.php
@@ -7,6 +7,53 @@
     <title>PHP Paskaita_4</title>
 </head>
 <body>
+<!-- 1. Sukurkite programą, kuri priklausomai nuo naršyklės (Chrome - "juoda", Mozzila - "raudona" ,  Edge - "melyna"), puslapis nusidažo atitinkama spalva. Taip pat, puslapyje priklausomai nuo naršyklės, turi atsirasti tos naršyklės logotipas.
+P.S užduotį jau įkelti į GitHub -->
+<?php 
+
+    if(strpos($_SERVER['HTTP_USER_AGENT'], 'Edg') !== FALSE){
+        $narsykle = "Edge";}
+    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE){
+        $narsykle =  "Firefox";}
+    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE){
+        $narsykle =  "Chrome";}
+
+      
+
+    if($narsykle == "Chrome"){
+        echo "Tu naudoji : ";
+        echo '<style> body,html {background:url(chrome.png) no-repeat center center fixed</style>';
+        echo '<style> body,html {background-color:black}</style>';
+        echo '<style> body,html {color:white}</style>';
+    }
+    else if($narsykle == "Firefox"){
+        echo "Tu naudoji : ";
+        echo '<style> body,html {background:url(mozzila.png) no-repeat center center fixed</style>';
+        echo '<style> body,html {background-color:red}</style>';
+        echo '<style> body,html {color:white}</style>';
+    }
+else if($narsykle == "Edge"){
+        echo "Tu naudoji : ";
+        echo '<style> body,html {background:url(edge.png) no-repeat center center fixed</style>';
+        echo '<style> body,html {background-color:blue}</style>';
+        echo '<style> body,html {color:white}</style>';
+    }
+
+echo $narsykle;
+
+
+
+
+
+   
+ 
+
+
+
+
+?>
+
+
 
 </body>
 </html>
